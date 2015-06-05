4fc46df11fc89b76233aafe61a72679e17faf4c8
Darwin Sanoy <DarwinJSgithub.services@sanoys.com>
2015-06-05 17:07:46 -0400

    first commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'RegDllView'
+$url = 'http://www.nirsoft.net/utils/dotnetresourcesextract.zip'
+$url64 = 'http://www.nirsoft.net/utils/dotnetresourcesextract.zip'
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
